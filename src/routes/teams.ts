2b6be6a6a6d038d5f072f326307b23ecbe74a16c
Jennyads <jennycristina.1998@gmail.com>
2023-05-29 18:06:51 -0300

    controller

--- src/routes/teams.ts
+++ src/routes/teams.ts
@@ -3,12 +3,6 @@ import { Router } from "express";
 import { TeamsController } from "../controllers";
 const routes = Router();
 
-// routes.get('/especifico/:uuid', CommitteeControllers.getCommittee);
-
-// routes.post('/createCommittee', CommitteeControllers.postCommittee);
-
-// routes.put('/modify/:uuid', CommitteeControllers.putCommittee);
-
 routes.get("/", TeamsController.getAllTeams)
 routes.get("/:termo", TeamsController.getTermoTeams)
 routes.post("/", TeamsController.postTeams)
